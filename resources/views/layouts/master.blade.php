d4336dbc9fa7e9fe5769b5db6f0172602b701269
Imran Bajwa <imranbajwa01@gmail.com>
2020-12-08 13:50:43 +0100

    - master layout added

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @section('sidebar')
+            This is the master sidebar.
+        @show
+
+        <div class="container">
+            @yield('content', 'Default Content')
+        </div>
+    </body>
+</html>
